0442606c75229d1d76712548fbee1e539a315bc3
AcidicArmadillo <lcb2021001@iiitl.ac.in>
2023-09-15 15:27:09 +0530

    Added project files

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+import { uploadFile } from './service/api';
+
+function App() {
+  const [file, setFile] = useState('');
+  const [result, setResult] = useState('');
+
+  const fileInputRef = useRef();
+
+  const url = 'https://images.unsplash.com/photo-1519638399535-1b036603ac77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80';
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  }, [file])
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  }
+
+  return (
+    <div className='container'>
+      <div className="wrapper rounded-md shadow-md opacity-90 bg-slate-200 flex justify-center items-center gap-3">
+        <h1 className=''>Simple file sharing!</h1>
+        <p>Upload and share the download link.</p>
+        
+        <button onClick={() => onUploadClick()} className='hover:bg-slate-700 transition-all bg-slate-500 rounded-md px-5 py-2 text-white'>Upload</button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+
+        <a href={result} target='_blank' className='text-blue-700 underline hover:text-blue-800 cursor-pointer'>{result}</a> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
